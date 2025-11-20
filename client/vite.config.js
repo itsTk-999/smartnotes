173d60f825e6b4d6f43b9d3afd10b1a9f59e20b0
lil tk <liltk0998@gmail.com>
2025-11-20 16:48:58 +0300

    Check 3

--- client/vite.config.js
+++ client/vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'http://localhost:5000' || 'https://smart-notes-i54i.onrender.com',
         changeOrigin: true,
         secure: false,
       }
